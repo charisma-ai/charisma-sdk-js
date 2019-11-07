8fc1da78d1d4367051f1dc48f1939d0f3f5173e3
Ben James <benhjames@sky.com>
2019-11-07 15:11:20 +0000

    fix: fix types for `EpisodeCompleteEvent`

--- src/types.ts
+++ src/types.ts
@@ -125,4 +125,6 @@ export type MessageEvent = {
 export interface EpisodeCompleteEvent {
   conversationId: ConversationId;
   impacts: Impact[];
+  completedEpisodeId: number;
+  nextEpisodeId: number | null;
 }
