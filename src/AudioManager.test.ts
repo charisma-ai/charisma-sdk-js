940e166ac148e76bb575a1f4b8967f8acb05ec69
Samuel Beard <sam@charisma.ai>
2024-08-01 12:35:25 +0100

    update tests

--- src/AudioManager.test.ts
+++ src/AudioManager.test.ts
@@ -32,17 +32,25 @@ describe("AudioManager", () => {
       normalVolumeLevel: 1,
       sttService: "browser",
       streamTimeslice: 100,
+      handleError: jest.fn(),
+      handleStartSTT: jest.fn(),
+      handleStopSTT: jest.fn(),
+      handleTranscript: jest.fn(),
     };
     const audioManager = new AudioManager(mockOptions);
     expect(audioManager).toBeInstanceOf(AudioManager);
   });
-
-  test("inputServiceStartListening should call startListening", () => {
+  // TODO better name
+  test("inputServiceStartListening should call startListening on audioInputsBrowser", () => {
     const mockOptions: AudioManagerOptions = {
       duckVolumeLevel: 0.2,
       normalVolumeLevel: 1,
       sttService: "browser",
       streamTimeslice: 100,
+      handleError: jest.fn(),
+      handleStartSTT: jest.fn(),
+      handleStopSTT: jest.fn(),
+      handleTranscript: jest.fn(),
     };
 
     const mockAudioInputsBrowserInstance = new MockAudioInputsBrowser();
@@ -51,12 +59,43 @@ describe("AudioManager", () => {
 
     // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access
     (audioManager as any).audioInputsBrowser = mockAudioInputsBrowserInstance;
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access
+    (audioManager as any).audioInputsService = mockAudioInputsServiceInstance;
 
     audioManager.startListening("test-token");
 
     expect(mockAudioInputsBrowserInstance.startListening).toHaveBeenCalled();
     expect(
       mockAudioInputsServiceInstance.startListening,
+    ).not.toHaveBeenCalled();
+  });
+
+  test("inputServiceStartListening should call startListening on audioInputsService", () => {
+    const mockOptions: AudioManagerOptions = {
+      duckVolumeLevel: 0.2,
+      normalVolumeLevel: 1,
+      sttService: "charisma/deepgram",
+      streamTimeslice: 100,
+      handleError: jest.fn(),
+      handleStartSTT: jest.fn(),
+      handleStopSTT: jest.fn(),
+      handleTranscript: jest.fn(),
+    };
+
+    const mockAudioInputsBrowserInstance = new MockAudioInputsBrowser();
+    const mockAudioInputsServiceInstance = new MockAudioInputsService();
+    const audioManager = new AudioManager(mockOptions);
+
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access
+    (audioManager as any).audioInputsBrowser = mockAudioInputsBrowserInstance;
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access
+    (audioManager as any).audioInputsService = mockAudioInputsServiceInstance;
+
+    audioManager.startListening("test-token");
+
+    expect(mockAudioInputsServiceInstance.startListening).toHaveBeenCalled();
+    expect(
+      mockAudioInputsBrowserInstance.startListening,
     ).not.toHaveBeenCalled();
   });
 });
